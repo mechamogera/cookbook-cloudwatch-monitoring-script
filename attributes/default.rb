af14e961bf7bb6946efe80628e12e261eb6a3214
mechamogera <mechamosura@gmail.com>
2013-07-08 09:01:31 +0900

    bug fix

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
 default["cloudwatch-monitoring-script"]["user"] = "ec2-user"
 default["cloudwatch-monitoring-script"]["group"] = "ec2-user"
 default["cloudwatch-monitoring-script"]["deploy_dir"] = "/home/ec2-user/script"
-default["cloudwatch-monitoring-script"]["cron_path"] = "--mem-util --swap-util --disk-space-util --disk-path=/ --from-cron"
+default["cloudwatch-monitoring-script"]["cron_options"] = "--mem-util --swap-util --disk-space-util --disk-path=/ --from-cron"
--- recipes/default.rb
+++ recipes/default.rb
@@ -29,6 +29,6 @@ end
 cron "cloudwatch-monitoring" do
   user node["cloudwatch-monitoring-script"]["user"]
   minute "*/5"
-  command %Q!#{node["cloudwatch-monitoring-script"]["deploy_dir"]}/aws-scripts-mon/mon-put-instance-data.pl #{node["cw-monitoring-script"]["cron_options"]}!
+  command %Q!#{node["cloudwatch-monitoring-script"]["deploy_dir"]}/aws-scripts-mon/mon-put-instance-data.pl #{node["cloudwatch-monitoring-script"]["cron_options"]}!
   action :create
 end
